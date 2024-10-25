e9790ee5a36e3bf9afc13c7238869a224a123ea7
himanshu <kumarehimanshu1@gmail.com>
2024-10-25 12:43:42 +0530

    initialized the project

---
+++ SafeVault/src/test/java/com/safevault/bankingapp/SafeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safevault.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
